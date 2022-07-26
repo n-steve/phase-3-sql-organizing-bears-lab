2563f13a44992b929c989346c68aa38dd59a6ccf
steve <steve.nguyen2013@gmail.com>
2022-07-26 14:14:53 -0700

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,sex) VALUES ('Mr. Chocolate', 'M');
+INSERT INTO bears (name,sex) VALUES ('Rowdy', 'F');
+INSERT INTO bears (name,sex) VALUES ('Sergeant Brown', 'M');
+INSERT INTO bears (name,sex) VALUES ('Tabitha', 'F');
+INSERT INTO bears (name,sex) VALUES ('Melissa', 'F');
+INSERT INTO bears (name,sex) VALUES ('Grinch', 'M');
+INSERT INTO bears (name,sex) VALUES ('Wendy', 'M');
+INSERT INTO bears (name,sex) VALUES (null, 'M')
